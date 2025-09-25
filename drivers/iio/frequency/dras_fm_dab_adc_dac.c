95e9f8206b859c913f06458cebec751ce278081e
zta1 <zutter@precisionwave.com>
2025-09-25 14:19:53 +0200

    add tx buffer gain

--- drivers/iio/frequency/dras_fm_dab_adc_dac.c
+++ drivers/iio/frequency/dras_fm_dab_adc_dac.c
@@ -566,6 +566,10 @@ static ssize_t dras_fm_dab_adc_dac_store(struct device *dev,
 		dras_fm_dab_adc_dac_write(st, ADDR_RX_DAB_BAND_BURST_LENGTH, (u32)val);
 		break;
 	case REG_RX_DAB_BAND_BURST_PERIOD:
+		if(val<1 || val>255){
+			ret = -EINVAL;
+			break;
+		}
 		dras_fm_dab_adc_dac_write(st, ADDR_RX_DAB_BAND_BURST_PERIOD, (u32)val);
 		break;
 	case REG_RX_DAB_MONITOR_BURST_LENGTH:
@@ -1385,7 +1389,7 @@ static ssize_t dras_fm_dab_adc_dac_show(struct device *dev,
 		val = dras_fm_dab_adc_dac_read(st, ADDR_RX_DAB_BAND_BURST_LENGTH);
 		break;
 	case REG_RX_DAB_BAND_BURST_PERIOD:
-		val = dras_fm_dab_adc_dac_read(st, ADDR_RX_DAB_BAND_BURST_PERIOD);
+		val = dras_fm_dab_adc_dac_read(st, ADDR_RX_DAB_BAND_BURST_PERIOD) & 0xFF;
 		break;
 	case REG_RX_DAB_MONITOR_BURST_LENGTH:
 		val = dras_fm_dab_adc_dac_read(st, ADDR_RX_DAB_MONITOR_BURST_LENGTH);
