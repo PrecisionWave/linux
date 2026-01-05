e82238c7da9928b50a5818485d699cc50df56105
zta1 <zutter@precisionwave.com>
2026-01-05 17:23:02 +0100

    260105: one time sync DTF

--- drivers/iio/frequency/dras_fm_dab_adc_dac.c
+++ drivers/iio/frequency/dras_fm_dab_adc_dac.c
@@ -261,6 +261,7 @@ enum chan_num{
 	REG_TX_DAB_TESTTONE_AMPLITUDE3,
 	//REG_TXBUF_ENABLE,
 	REG_EN_DTF_SEQUENCER,
+	REG_START_DTF_SEQUENCER,
 	REG_TX_BUFFER_MASK,
 	REG_RX_BUFFER_MASK,
 	REG_TX_BUFFER_MUTE_CHANNEL_MASK,
@@ -332,6 +333,7 @@ struct dras_fm_dab_adc_dac_state {
 	bool			rf_mute;
 	bool			is_remote;
 	u32			fm_testtone_inc[5];
+	u32			en_dtf_seq;
 
 	struct clk		*dsp_clk;
 	struct gpio_desc	*clk_ce_gpio;
@@ -1184,12 +1186,28 @@ static ssize_t dras_fm_dab_adc_dac_store(struct device *dev,
 		dras_fm_dab_adc_dac_write(st, ADDR_TX_BUFFER_MUTE_CHANNEL_MASK, (u32)uval);
 		break;
 	case REG_EN_DTF_SEQUENCER:
+		if(val<0 || val>3){
+			ret = -EINVAL;
+			break;
+		}
+		st->en_dtf_seq = val;
+		if(val==2)
+			val=3;
+		temp32 = dras_fm_dab_adc_dac_read(st, ADDR_RX_DAB_BAND_BURST_LENGTH) & ~(0x3<<28);
+		temp32 += (u32)val << 28;
+		dras_fm_dab_adc_dac_write(st, ADDR_RX_DAB_BAND_BURST_LENGTH, temp32);
+		break;
+	case REG_START_DTF_SEQUENCER:
+		if(st->is_remote){
+			ret = -ENODEV;
+			break;
+		}
 		if(val<0 || val>1){
 			ret = -EINVAL;
 			break;
 		}
-		temp32 = dras_fm_dab_adc_dac_read(st, ADDR_RX_DAB_BAND_BURST_LENGTH) & ~(0x1<<28);
-		temp32 += (u32)val << 28;
+		temp32 = dras_fm_dab_adc_dac_read(st, ADDR_RX_DAB_BAND_BURST_LENGTH) & ~(0x1<<30);
+		temp32 += (u32)val << 30;
 		dras_fm_dab_adc_dac_write(st, ADDR_RX_DAB_BAND_BURST_LENGTH, temp32);
 		break;
 	case REG_TX1_BUFFER_MUTES_FM_CHANNELS:
@@ -1767,7 +1785,10 @@ static ssize_t dras_fm_dab_adc_dac_show(struct device *dev,
 		ret = sprintf(buf, "%08x\n", temp32);
 		break;
 	case REG_EN_DTF_SEQUENCER:
-		val = (dras_fm_dab_adc_dac_read(st, ADDR_RX_DAB_BAND_BURST_LENGTH) >> 28) & 0x1;
+		val = st->en_dtf_seq;
+		break;
+	case REG_START_DTF_SEQUENCER:
+		val = (dras_fm_dab_adc_dac_read(st, ADDR_RX_DAB_BAND_BURST_LENGTH) >> 30) & 0x1;
 		break;
 	case REG_TX1_BUFFER_MUTES_FM_CHANNELS:
 		val = (dras_fm_dab_adc_dac_read(st, ADDR_RX_DAB_BAND_BURST_LENGTH) >> 26) & 0x1;
@@ -2109,6 +2130,11 @@ static IIO_DEVICE_ATTR(enable_dtf_sequencer, S_IRUGO | S_IWUSR,
 			dras_fm_dab_adc_dac_show,
 			dras_fm_dab_adc_dac_store,
 			REG_EN_DTF_SEQUENCER);
+
+static IIO_DEVICE_ATTR(start_dtf_sequencer, S_IRUGO | S_IWUSR,
+			dras_fm_dab_adc_dac_show,
+			dras_fm_dab_adc_dac_store,
+			REG_START_DTF_SEQUENCER);
 
 static IIO_DEVICE_ATTR(tx1_buffer_mutes_fm_channels, S_IRUGO | S_IWUSR,
 			dras_fm_dab_adc_dac_show,
@@ -2489,6 +2515,7 @@ static struct attribute *dras_fm_dab_adc_dac_attributes[] = {
 	&iio_dev_attr_rx_buffer_mask.dev_attr.attr,
 	&iio_dev_attr_tx_buffer_mute_channel_mask.dev_attr.attr,
 	&iio_dev_attr_enable_dtf_sequencer.dev_attr.attr,
+	&iio_dev_attr_start_dtf_sequencer.dev_attr.attr,
 	&iio_dev_attr_tx1_buffer_mutes_fm_channels.dev_attr.attr,
 	&iio_dev_attr_tx2_buffer_mutes_fm_channels.dev_attr.attr,
 	&iio_dev_attr_tx1_buffer_mutes_dab_channels.dev_attr.attr,
